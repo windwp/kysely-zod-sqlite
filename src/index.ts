9d439d204c020952dca1fb58b42d7de6665bb4c3
windwp <windwp@users.noreply.github.com>
2023-07-27 20:47:11 +0700

    fix: add function pid

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
+import { uid } from 'uid';
+
 export { SqliteApi, PQuery } from './SqliteApi';
 export { zBoolean, zJsonString } from './helpers/zod';
+
+export function pid(prefix: string, length = 10) {
+  return `${prefix}_${uid(length)}`;
+}
