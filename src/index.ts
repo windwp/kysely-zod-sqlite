09ae424071f594abcdb71771095abd3b78329d8a
windwp <windwp@users.noreply.github.com>
2023-07-28 14:38:24 +0700

    fix: export zdate

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { uid } from 'uid';
 
 export { SqliteApi, PQuery } from './SqliteApi';
-export { zBoolean, zJsonString } from './helpers/zod';
+export { zBoolean, zJsonString, zDate } from './helpers/zod';
 
-export function pid(prefix: string, length = 10) {
+export function pid(prefix?: string, length = 16) {
+  if (!pid) return uid(length);
   return `${prefix}_${uid(length)}`;
 }
