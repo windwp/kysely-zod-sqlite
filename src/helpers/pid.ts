0ef3a1b5710c089c39abb553173b9c11ae77a0ad
windwp <windwp@users.noreply.github.com>
2023-07-29 18:32:20 +0700

    fix: pid
    Do I need that prefix on id?

--- src/helpers/pid.ts
+++ src/helpers/pid.ts
@@ -1,5 +1,5 @@
 import { uid } from 'uid';
 export function pid(prefix?: string, length = 18) {
   if (!prefix) return uid(length);
-  return `${prefix}_${uid(length)}`;
+  return `${prefix}-${uid(length)}`;
 }
