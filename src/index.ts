0761b20c4d02ec56bab754ded31722a7ad9008b2
windwp <windwp@users.noreply.github.com>
2023-07-30 15:43:40 +0700

    fix: export zod

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { SqliteApi, PTable } from './SqliteApi';
-export { zBoolean, zJsonSchema, zDate, zJsonObject } from './helpers/zod';
+export * from './helpers/zod';
 export { uid } from './helpers/uid';
 export { sql } from 'kysely';
 
