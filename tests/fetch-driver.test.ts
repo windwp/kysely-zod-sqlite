9660201068fc924874e7e9e90fccc7b505ddf468
windwp <windwp@users.noreply.github.com>
2023-09-17 14:19:25 +0700

    fix: fetch-driver

--- tests/fetch-driver.test.ts
+++ tests/fetch-driver.test.ts
@@ -1,24 +1,23 @@
 import { describe, it } from 'vitest';
 import dotenv from 'dotenv';
-dotenv.config();
+dotenv.config
 import { runTest } from './sharedTest';
 import { TestApi } from './TestApi';
 import { FetchDriver } from '../src/driver/fetch-driver';
 import loglevel from 'loglevel';
 import { dbSchema } from './kysely-schema';
 
-const api = new TestApi({
-  config: { logger: loglevel },
-  schema: dbSchema,
-  driver: new FetchDriver({
-    apiKey: process.env.API_KEY!,
-    apiUrl: process.env.API_URL!,
-    logger: loglevel,
-  }),
-});
-
 describe('FetchDriver', () => {
   if (process.env.CI) {
+    const api = new TestApi({
+      config: { logger: loglevel },
+      schema: dbSchema,
+      driver: new FetchDriver({
+        apiKey: process.env.API_KEY!,
+        apiUrl: process.env.API_URL!,
+        logger: loglevel,
+      }),
+    });
     runTest(api);
   } else {
     it('should ', async () => {});
