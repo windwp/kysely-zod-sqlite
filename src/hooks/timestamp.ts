d24552a613761483647ff8afdb44994d1729536c
windwp <windwp@users.noreply.github.com>
2023-11-19 15:07:14 +0700

    fix: timestamp

--- src/hooks/timestamp.ts
+++ src/hooks/timestamp.ts
@@ -3,12 +3,15 @@ import { PHooks } from '../types';
 export function hookTimeStamp(key: { createdAt: string; updatedAt: string }) {
   return {
     onInsert(value: any, ctx) {
-      if (ctx.schema.shape[key.createdAt]) value[key.createdAt] = new Date();
-      if (ctx.schema.shape[key.updatedAt]) value[key.updatedAt] = new Date();
+      if (!value[key.createdAt] && ctx.schema.shape[key.createdAt])
+        value[key.createdAt] = new Date();
+      if (!value[key.updatedAt] && ctx.schema.shape[key.updatedAt])
+        value[key.updatedAt] = new Date();
       return value;
     },
     onUpdate(value: any, ctx: any) {
-      if (ctx.schema.shape[key.updatedAt]) value[key.updatedAt] = new Date();
+      if (!value[key.updatedAt] && ctx.schema.shape[key.updatedAt])
+        value[key.updatedAt] = new Date();
     },
   } satisfies PHooks;
 }
