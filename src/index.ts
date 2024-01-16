e7d47d3c03c860afb42c3f4e540df49c02d87ddc
windwp <windwp@users.noreply.github.com>
2024-01-16 18:45:06 +0700

    fix: export type

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,14 @@ export type { Query, TableDefinition } from './types';
 export { SqliteApi } from './api/sqlite-api';
 export { PostgresApi } from './api/postgres-api';
 export { PTable } from './api/papi';
-export type { DbDriverConfig, ZodSchemaToKysely } from './types';
+export type {
+  FetchDriverConfig,
+  BettterDriverConfig,
+  DbDriverConfig,
+  ZodSchemaToKysely,
+  QueryRelations,
+  ApiConfig,
+} from './types';
 export * from './helpers/zod';
 export { createKyselySqlite } from './creator/sqlite';
 export { sql } from 'kysely';
